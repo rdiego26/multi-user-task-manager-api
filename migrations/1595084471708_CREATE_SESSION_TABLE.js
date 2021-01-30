c06ae81f46a9e992f61a55f53d6a8eed2856f981
ramos <rdiego26@gmail.com>
2021-01-30 13:54:17 -0200

    ADD migration - session table

---
+++ migrations/1595084471708_CREATE_SESSION_TABLE.js
@@ -0,0 +1,29 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = pgm => {
+    const SQL = `
+		create table IF NOT EXISTS "session"
+			(
+					id uuid not null
+						constraint session_pkey
+							primary key,
+					user_id uuid not null
+						constraint fk_session_user
+							references "user"
+					token text not null
+						constraint session_token_key
+							unique,
+					expires_at timestamp with time zone default now() not null
+			);
+	`;
+    pgm.sql(SQL);
+};
+
+exports.down = pgm => {
+    const SQL = `
+			DROP TABLE IF EXISTS "session";
+    `;
+    pgm.sql(SQL);
+};
