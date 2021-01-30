2720ff2f3eae45054a07c9df6b1888e0d540d023
ramos <rdiego26@gmail.com>
2021-01-30 14:28:37 -0200

    ADD session service

---
+++ src/services/SessionService.js
@@ -0,0 +1,82 @@
+const dayJs = require('dayjs');
+const uuid = require('uuid-v4');
+const Sequelize = require('sequelize');
+const connection = require('../utils/poolConnection');
+const UserModel = require('../models/UserModel').init(connection, Sequelize.DataTypes);
+const { Op } = Sequelize;
+
+class SessionService {
+	constructor(sessionModel) {
+		this.sessionModel = sessionModel;
+	}
+
+	async fetchUserData(token) {
+		const query = { token };
+
+		return this.sessionModel
+			.scope('find')
+			.findOne({
+				where: query,
+				include: [{
+					model: UserModel,
+					as: 'user',
+				}],
+			});
+
+	}
+
+	async create(userId) {
+		const item = {
+			token: uuid(),
+			userId,
+			expiresAt: dayJs().add(10, 'days'),
+		};
+
+		return this.sessionModel
+			.scope('create')
+			.create(item);
+	}
+
+	async findActiveToken(token) {
+		const query = {
+			token,
+			expiresAt: {
+				[Op.gt]: connection.fn('NOW'),
+			},
+		};
+
+		return this.sessionModel
+			.scope('find')
+			.findOne({
+				where: query,
+                include: [{
+                    model: UserModel,
+                    as: 'user',
+                }],
+			});
+	}
+
+    async increment(session) {
+      const query = {
+          token: session.token
+      };
+
+      const newData = {
+          expiresAt: dayJs(session.expiresAt).add(1, 'days')
+			};
+
+      return this.sessionModel.update(newData, {
+          where: query,
+          returning: true,
+          plain: true,
+      });
+    }
+
+	async deleteByUserId(userId) {
+		const query = { userId };
+		return this.sessionModel.destroy({ where: query });
+	}
+
+}
+
+module.exports = SessionService;
