e30668ac5bfbabcdd06f3841c279a97cd3ce58d7
ramos <rdiego26@gmail.com>
2021-01-30 14:19:54 -0200

    ADD model for task

---
+++ src/models/TaskModel.js
@@ -0,0 +1,49 @@
+const Sequelize = require('sequelize');
+
+class TaskModel extends Sequelize.Model {
+    static init(sequelize, DataTypes) {
+
+        return super.init(
+            {
+                id: {
+                    type: Sequelize.UUID, primaryKey: true, defaultValue: DataTypes.UUIDV4,
+                },
+                projectId: {
+                    type: DataTypes.UUID,
+                    allowNull: false,
+                    field: 'project_id',
+                    references: {
+                        model: 'project',
+                        key: 'id',
+                    },
+                },
+	              description: {
+                    type: DataTypes.STRING(200), allowNull: false,
+                },
+                createdAt: {
+                    type: DataTypes.DATE, field: 'created_at',
+                },
+	              finishedAt: {
+                  type: DataTypes.DATE, field: 'finished_at',
+                },
+            },
+            {
+                sequelize,
+                freezeTableName: true,
+                createdAt: false,
+                updatedAt: false,
+                tableName: 'task',
+                scopes: {
+                    create: {
+                        attributes: ['projectId', 'description'],
+                    },
+                    find: {
+                        attributes: ['projectId', 'description', 'createdAt', 'finishedAt'],
+                    },
+                },
+            }
+        );
+    }
+}
+
+module.exports = TaskModel;
