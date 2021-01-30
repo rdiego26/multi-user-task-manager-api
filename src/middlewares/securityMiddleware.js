5bbcccadc28ef9992b972e336917b2116fff15d0
ramos <rdiego26@gmail.com>
2021-01-30 17:42:37 -0200

    IMPROVE security middleware

--- src/middlewares/securityMiddleware.js
+++ src/middlewares/securityMiddleware.js
@@ -1,19 +1,23 @@
+const { StatusCodes } = require('http-status-codes');
 const sessionServiceInstance = require('../services').SessionService;
+
+const PUBLIC_ROUTES = ['/api/healthCheck', '/api/login'];
 
 module.exports = {
   checkToken: async (req, res, next) => {
     const token = req.headers['x-access-token'] || null;
     const requestURL = req.originalUrl;
+    const method = req.method;
 
-    if (requestURL === '/api/healthCheck' || requestURL === '/api/login') {
+    if (PUBLIC_ROUTES.includes(requestURL) || (requestURL === '/api/user' && method === 'POST')) {
       next();
     } else if (!token) {
-      res.status(401).end();
+      res.status(StatusCodes.UNAUTHORIZED).end();
     } else {
       const fetchedData = await sessionServiceInstance.findActiveToken(token);
 
       if (!fetchedData) {
-        res.status(401).end();
+        res.status(StatusCodes.UNAUTHORIZED).end();
       } else {
         await sessionServiceInstance.increment(fetchedData);
         next();
