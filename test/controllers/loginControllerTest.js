4aa55e1e520e387a7e415343a0a81251b4dc6515
ramos <rdiego26@gmail.com>
2021-01-30 18:05:23 -0200

    ADD more tests for login controller

--- test/controllers/loginControllerTest.js
+++ test/controllers/loginControllerTest.js
@@ -3,6 +3,9 @@ const { StatusCodes } = require('http-status-codes');
 const assert = require('chai').assert;
 const app = require('../../src/index');
 const faker = require('faker');
+const sinon = require("sinon");
+const UserService = require('../../src/services').UserService;
+const SessionService = require('../../src/services').SessionService;
 
 describe('Login Controller', () => {
 
@@ -37,6 +40,51 @@ describe('Login Controller', () => {
 
 		assert.ok(response.body.errors.length === 1);
 		assert.propertyVal(response.body?.errors[0], 'message', "should have required property 'email'");
+	});
+
+	it('should receive properly response when send valid data', async () => {
+		let userStub = sinon.stub(UserService, 'findWithCredentials').returns(null);
+
+		await request(app)
+			.post('/api/login')
+			.send({ email: faker.internet.email(), password: faker.internet.password() })
+			.set('Accept', 'application/json')
+			.expect('Content-Type', /json/)
+			.expect(StatusCodes.UNAUTHORIZED);
+		await userStub.restore();
+	});
+
+	it('should receive properly response when send valid data', async () => {
+		const mockedUser = {
+			id: faker.random.uuid(),
+			name: faker.name.findName(),
+			email: faker.internet.email(),
+			password: faker.internet.password()
+		};
+
+		const mockedSession = {
+			id: faker.random.uuid(),
+			userId: mockedUser.id,
+			token: faker.random.alphaNumeric(10),
+			expiresAt: faker.date.future()
+		};
+
+		let userStub = sinon.stub(UserService, 'findWithCredentials').returns(mockedUser);
+		let sessionStub = sinon.stub(SessionService, 'create').returns(mockedSession);
+
+		let response = await request(app)
+			.post('/api/login')
+			.send({ email: mockedUser.email, password: mockedUser.password })
+			.set('Accept', 'application/json')
+			.expect('Content-Type', /json/)
+			.expect(StatusCodes.OK);
+
+		assert.property(response.body, 'id');
+		assert.property(response.body, 'userId');
+		assert.property(response.body, 'token');
+		assert.property(response.body, 'expiresAt');
+		await userStub.restore();
+		await sessionStub.restore();
 	});
 
 });
